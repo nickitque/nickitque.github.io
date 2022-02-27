be175cc776cabd72f64a92b55a470dffa6072a65
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-28 00:29:03 +0300

    Added template for tests

--- landing/main/urls.py
+++ landing/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+"""There you can change url path and its name."""
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('about', views.about, name='about-us'),
-    path('shop', views.shop, name='shop-example'),
+    path('skills', views.shop, name='shop-example'),
     path('create-test-cases', views.create_tcs, name='create-test-cases'),
     path('create-new-el-indb', views.test, name='create-new'),
 ]
