77fa994f5316e0355072b31a20ddbbd4ee64b32d
Nikita <nickitque.39@gmail.com>
2023-12-03 18:34:25 +0300

    Refactoring the index file

---
+++ course/script.js
@@ -0,0 +1,7 @@
+
+const btns = document.querySelectorAll('button');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+console.log(btns[1].classList.add('red'));
+console.log(btns[3].classList.remove('blue'));
